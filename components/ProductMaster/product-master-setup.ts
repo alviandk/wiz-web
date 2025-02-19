2b49c8f2ced199f15441f65da46117bd6893c62a
“annisa.syuhada” <annisa.syuhada@dto.kemkes.go.id>
2025-02-20 03:20:43 +0700

    Update menu edit product master

--- components/ProductMaster/product-master-setup.ts
+++ components/ProductMaster/product-master-setup.ts
@@ -1,13 +1,32 @@
+import { useProductStore } from '~/stores/product'
+
 export const useProductMaster = () => {
+  const { isEditableProduct, setIsEditableProduct } = useProductStore()
+
   const isModalBack = ref(false)
+  const isModalConfirmationDelete = ref(false)
 
   function onToggleModalBack() {
     isModalBack.value = !isModalBack.value
   }
 
+  function onToggleModalConfirmationDelete() {
+    isModalConfirmationDelete.value = !isModalConfirmationDelete.value
+  }
+
+  function onChangeEditProduct() {
+    setIsEditableProduct(true)
+  }
+
   return {
     isModalBack,
     onToggleModalBack,
+
+    isModalConfirmationDelete,
+    onToggleModalConfirmationDelete,
+
+    isEditableProduct,
+    onChangeEditProduct,
   }
 }
 
--- stores/product.ts
+++ stores/product.ts
@@ -1,18 +1,26 @@
-export const useProductCategoryStore = defineStore('product-category', () => {
+export const useProductStore = defineStore('product', () => {
+  const isEditableProduct = ref(false)
   const isEditableCategory = ref(false)
+
+  function setIsEditableProduct(param: boolean) {
+    isEditableProduct.value = param
+  }
 
   function setIsEditableCategory(param: boolean) {
     isEditableCategory.value = param
   }
 
   return {
+    isEditableProduct,
+    setIsEditableProduct,
+
     isEditableCategory,
     setIsEditableCategory,
   }
 })
 
 if (import.meta.hot) {
-  import.meta.hot.accept(acceptHMRUpdate(useProductCategoryStore, import.meta.hot))
+  import.meta.hot.accept(acceptHMRUpdate(useProductStore, import.meta.hot))
 }
 
-export default { useProductCategoryStore }
+export default { useProductStore }
